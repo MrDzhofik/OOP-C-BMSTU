c0231c71671b3d360b976e48eb8e09146d91569d
MrDzhofik <djafarovemil04@mail.ru>
2022-03-07 18:47:30 +0300

    Finished 5 Laboratory.

--- Laboratory5/Laboratory5/Laboratory5.cpp
+++ Laboratory5/Laboratory5/Laboratory5.cpp
@@ -3,15 +3,87 @@
 
 #include <iostream>
 #include "locale.h"
+#include "string.h"
+#include "stdlib.h"
 
 using namespace std;
 
 int main()
 {
-    char str;
+    char curr[50], ans[50]{}, temp[50]{}, * ptr, * deleted[100]{};
+    int n = 0, i = 0, j = 0, k = 0;
+    bool flag = false, first = true;
     setlocale(0, "russian");
-    cin >> str;
-    cout << str;
+    cin.get(curr, 50);
+    ptr = curr;
+    while (curr[i - 1] != '\0') {
+        if (curr[i] == 'e') {
+            if (!flag) {
+                flag = true;
+                n++;
+            }
+            else {
+                n = 0;
+                while ((curr[i] != ' ') && (curr[i] != '\0')) {
+                    i++;
+                }
+                j++;
+                ptr = strchr(ptr + 1, curr[i]);
+            }
+       }
+        else {
+            if ((curr[i] == ' ') || (curr[i] == '\0')) {
+                if ((!flag) || ((curr[i - 1] = 'e') && (curr[i - 2] != 'e'))) {
+                    strncpy_s(temp, ptr, n + 1);
+                    strcat_s(ans, temp);
+                    ptr = strchr(ptr + 1, ' ');
+                    n = 0;
+                    
+                }
+                else {
+                    ptr = strchr(ptr + 1, ' ');
+                }
+            }
+            else {
+                n++;
+                flag = false;
+            }
+        }
+        i++;
+    }
+    i = 0;
+    flag = false;
+    while (curr[i] != '\0') {
+        if (curr[i] != 'e') {
+            flag = false;
+        }
+        else {
+            if (curr[i] == 'e') {
+                if (flag) {
+                    while ((curr[i] != ' ') && (i != -1)) {
+                        i--;
+                    }
+                    deleted[k] = &curr[i + 1];
+                    k++;
+                    i++;
+                    while ((curr[i] != ' ') && (curr[i] != '\0')) {
+                        i++;
+                    }
+                }
+                else {
+                    flag = true;
+                }
+            }
+        }
+        i++;
+    }
+    cout << "Исходная строка: " << curr << endl;
+    cout << "Полученная строка: " << ans << endl;
+    cout << "Адрес(a): ";
+    for (i = 0; i < j; i++) {
+        //printf_s('%c ', deleted[i]);
+        cout << &deleted[i] << ' ';
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
